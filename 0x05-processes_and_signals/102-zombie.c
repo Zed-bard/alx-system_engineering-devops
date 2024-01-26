66511b5cd10be6018c781032c5aabbe98d68caaa
Zed-bard <zerihunbard@gmail.com>
2024-01-26 10:38:31 -0800

    102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,8 +1,3 @@
-/*
- * File: 102-zombie.c
- * Author: Zerihun Shiferaw
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -12,47 +7,49 @@
 /**
  * infinite_while - Run an infinite while loop.
  *
- * This function runs an infinite while loop by repeatedly calling sleep(1).
+ * Description:
+ *   Run an infinite while loop by repeatedly calling sleep(1).
  *
  * Return: Always 0.
  */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 /**
  * main - Creates five zombie processes.
  *
- * This function creates five child processes using fork(). The parent process
- * prints the PID of the created zombie process, waits for 1 second, and repeats
- * this process five times. The child processes exit immediately.
+ * Description:
+ *   This function creates five child processes using fork().The parent process
+ *   prints the PID of the created zombie process, waits for 1 2nd,and repeats
+ *   this process five times. The child processes exit immediately.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    pid_t pid;
-    char count = 0;
+	pid_t pid;
+	char count = 0;
 
-    while (count < 5)
-    {
-        pid = fork();
-        if (pid > 0)
-        {
-            printf("Zombie process created, PID: %d\n", pid);
-            sleep(1);
-            count++;
-        }
-        else
-            exit(0);
-    }
+	while (count < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			count++;
+		}
+		else
+			exit(0);
+	}
 
-    infinite_while();
+	infinite_while();
 
-    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
