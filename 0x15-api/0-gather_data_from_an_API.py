c339732d63f30f014df408111a27bcbb2a55898e
Zed-bard <zerihunbard@gmail.com>
2024-04-22 11:10:56 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,39 +1,14 @@
 #!/usr/bin/python3
+"""Returns information about his/her to-do list progress."""
 import requests
 import sys
 
-def get_employee_progress(employee_id):
-    # Base URL for the API
-    api_url = "https://jsonplaceholder.typicode.com"
-
-    # Get the user data
-    user_url = f"{api_url}/users/{employee_id}"
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-
-    # Get the TODO list for the user
-    todos_url = f"{api_url}/todos?userId={employee_id}"
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
-
-    # Calculate progress
-    total_tasks = len(todos_data)
-    completed_tasks = sum(task['completed'] for task in todos_data)
-
-    # Print out the employee progress
-    print(f"Employee {user_data['name']} is done with tasks({completed_tasks}/{total_tasks}):")
-
-    # Print out the titles of completed tasks
-    for task in todos_data:
-        if task['completed']:
-            print(f"\t {task['title']}")
-
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        try:
-            employee_id = int(sys.argv[1])
-            get_employee_progress(employee_id)
-        except ValueError:
-            print("Please provide a valid integer for the employee ID.")
-    else:
-        print("Usage: python3 script.py <employee_id>")
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
+    todos = requests.get(url + 'todos', params={"userId": sys.argv[1]}).json()
+
+    completed = [todo for todo in todos if todo.get("completed") is True]
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user.get("name"), len(completed), len(todos)))
+    [print('\t {}'.format(c.get("title"))) for c in completed]
